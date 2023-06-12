4c78bfe867472355fce42f612f427f85c584d37a
pkolt <pkoltyshev@gmail.com>
2023-06-12 17:55:28 +0400

    Fix strict assert

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,5 @@
 import { describe, it } from 'node:test';
-import assert from 'node:assert';
+import assert from 'node:assert/strict';
 import { unified } from 'unified';
 import remarkParse from 'remark-parse';
 import remarkGfm from 'remark-gfm';
